6c6b720264c95c92f4d736e4ecec1da682e32602
Корякова Виктория <118850468+vkoryakova@users.noreply.github.com>
2023-05-22 19:22:24 +0300

    Add files via upload

--- ToursAppKoryakova23/MainWindow.xaml.cs
+++ ToursAppKoryakova23/MainWindow.xaml.cs
@@ -26,6 +26,21 @@ namespace ToursAppKoryakova23
             MainFrame.Navigate(new HotelsPage());
             Manager.MainFrame = MainFrame;
         }
+        private void BtnBack_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.GoBack();
+        }
+        private void MainFrame_ContentRendered(object sender, EventArgs e)
+        {
+            if(MainFrame.CanGoBack)
+            {
+                BtnBack.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                BtnBack.Visibility = Visibility.Hidden;
+            }
+        }
 
     }
 }
